36c54716653be3c93c81ba52a92bfd19e1698b61
liu_lei <liu_lei_programmer@163.com>
2019-01-21 16:36:26 +0800

    用台式机测试序列化结果

--- serializeCompare/src/main/java/com/liulei/fly/protostuff/ProtoBufUtil.java
+++ serializeCompare/src/main/java/com/liulei/fly/protostuff/ProtoBufUtil.java
@@ -2,7 +2,6 @@ package com.liulei.fly.protostuff;
 
 import io.protostuff.LinkedBuffer;
 import io.protostuff.ProtobufIOUtil;
-import io.protostuff.ProtostuffIOUtil;
 import io.protostuff.Schema;
 import io.protostuff.runtime.RuntimeSchema;
 
