f1071e4d10953c97d1603dba78e9f233dfaac50b
liu_lei <liu_lei_programmer@163.com>
2019-01-23 11:16:11 +0800

    调用服务器中间件服务

---
+++ middleservice/src/main/java/com/liulei/fly/middleservice/MiddleserviceApplication.java
@@ -0,0 +1,14 @@
+package com.liulei.fly.middleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleserviceApplication.class, args);
+    }
+
+}
+
