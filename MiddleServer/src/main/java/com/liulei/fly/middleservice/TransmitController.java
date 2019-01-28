7dafaa69940b1d61368eedb23a7b45f89f825887
liu_lei <liu_lei_programmer@163.com>
2019-01-28 21:14:03 +0800

    相应改变

--- MiddleServer/src/main/java/com/liulei/fly/middleservice/TransmitController.java
+++ MiddleServer/src/main/java/com/liulei/fly/middleservice/TransmitController.java
@@ -33,28 +33,29 @@ public class TransmitController {
 
     @ResponseBody
     @RequestMapping(path = "/transmit")
-    public String transmit(@RequestBody Map<String, String> map) throws IOException, NoSuchFieldException, IllegalAccessException {
+    public String transmit(@RequestBody Map<String, Object> map) throws IOException, NoSuchFieldException, IllegalAccessException {
         long s0 = System.currentTimeMillis();
-        String ip = map.get("ip");
-        int port = Integer.valueOf(map.get("port"));
+        String ip = map.get("ip").toString();
+        System.out.println("获取到请求；请求转发服务地址为" + ip);
+        int port = Integer.valueOf(map.get("port").toString());
         MyHttpClient myHttpClient = new MyHttpClient(ip, port);
         myHttpClient.setCookies(getCookie(map));
         Map<String,String> paras = new HashMap<String,String>();
         String param = getParam(map);
         paras.put("param", param);
-        String url = map.get("url");
+        String url = map.get("url").toString();
         long s1 = System.currentTimeMillis();
         String result = myHttpClient.doPost(url,paras);
         return "中间服务器耗时：" + (s1 - s0) + "||服务器返回数据为：" + result;
     }
 
-    private static String getCookie(Map<String, String> map){
+    private static String getCookie(Map<String, Object> map){
         Token token = new Token();
         Date date=new Date();
-        token.setCookie(map.get("cookie"));
-        token.setCompCode(map.get("compCode"));
-        token.setLoginCompCode(map.get("loginCompCode"));
-        token.setUserName(map.get("userName"));
+        token.setCookie(map.get("cookie").toString());
+        token.setCompCode(map.get("compCode").toString());
+        token.setLoginCompCode(map.get("loginCompCode").toString());
+        token.setUserName(map.get("userName").toString());
         map.remove("cookie");
         map.remove("userName");
         map.remove("loginCompCode");
@@ -63,7 +64,7 @@ public class TransmitController {
         return tokenString;
     }
 
-    private static String getParam(Map<String, String> map) throws UnsupportedEncodingException, IllegalAccessException, NoSuchFieldException {
+    private static String getParam(Map<String, Object> map) throws UnsupportedEncodingException, IllegalAccessException, NoSuchFieldException {
         SerializeUtil.initProtoParam(false);
         Param param = new Param();
         Class clazz = Param.class;
